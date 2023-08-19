d6aeff7d8b9441018c45a89cdb6ba28176ac6c1b
manuelrodriguezgodoy <manuelpinamar@hotmail.com>
2023-08-19 16:30:07 -0300

    products

--- js/products.js
+++ js/products.js
@@ -1,42 +1,36 @@
 // products.js
 document.addEventListener("DOMContentLoaded", function () {
-    const url = "https://japceibal.github.io/emercado-api/cats_products/101.json";
-    const productList = document.querySelector("#product-list");
-  
-    fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error("Error al hacer la solicitud.");
-        }
-        return response.json();
-      })
-      .then(data => {
-        const products = data.products;
-  
-         products.forEach(product => {
-          const productItem = document.createElement("div");
-          productItem.classList.add("product-item");
-  
-          productItem.innerHTML = 
-           `
+  const url = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+  const productList = document.querySelector("#product-list");
+
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error al hacer la solicitud.");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const products = data.products;
+
+      products.forEach((product) => {
+        const productItem = document.createElement("div");
+        productItem.classList.add("product-item");
+
+        productItem.innerHTML = `
             <img src="${product.image}" alt="${product.name}">
-            <h2 id="encabezado">${product.name}</h2>
-            <p id="Descripción">${product.description}</p>
-            <div id="testeo">
-              <p><b>Precio:</b> ${product.cost} ${product.currency}</p>
-              <p><b>Vendidos:</b> ${product.soldCount}</p>
-              <div id="botonCarrito">
-              <button id="carritoCompras"><b><span>Comprar</span></b></button>
-              <div class='testeo10'></div>
-              </div>
-            </div>
-            `
-          ;
-  
-          productList.appendChild(productItem);
-        });
-      })
-      .catch(error => {
-        console.error("Error:", error);
+            <h2 class="encabezado">${product.name}</h2>
+            <div class="product-gradiant"></div>
+            <p class="precio-producto">$${product.cost} ${product.currency}</p>
+            <p class="descripcion-producto">${product.description}</p>
+            <button class="boton-producto">Comprar</button>
+            <p class="vendidos-producto">Vendidos: ${product.soldCount}</p>
+            `;
+
+        productList.appendChild(productItem);
       });
-  });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
